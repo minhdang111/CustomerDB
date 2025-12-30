7b05c0b68c1b3870114afd9d48b0f933f594cae2
minhdang111 <ongminhdang456@gmail.com>
2025-12-30 10:47:42 -0600

    Add files via upload

---
+++ customerdb/src/main/java/com/example/customerdb/CustomerdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.customerdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerdbApplication.class, args);
+	}
+
+}
